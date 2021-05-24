ead5d227504d3984cd1d67d829e3988db3794462
Edward Matthew <edward.kurniawan@binus.ac.id>
2021-05-24 18:23:30 +0700

    modified Color class

--- src/com/chess/engine/Color.java
+++ src/com/chess/engine/Color.java
@@ -2,6 +2,18 @@ package com.chess.engine;
 
 // using an enum primarily because it is typesafe
 public enum Color {
-    WHITE,
-    BLACK
+    WHITE {
+        @Override
+        public int getDirection() {
+            return -1;
+        }
+    },
+    BLACK {
+        @Override
+        public int getDirection() {
+            return 1;
+        }
+    };
+
+    public abstract int getDirection();
 }
