e3e81b5fcdbfe2b53df8f92a28d36bc155307ca8
Edward Matthew <edward.kurniawan@binus.ac.id>
2021-06-14 01:01:15 +0700

    Optimized and cleaned up code

--- src/com/chess/engine/Color.java
+++ src/com/chess/engine/Color.java
@@ -1,5 +1,9 @@
 package com.chess.engine;
 
+import com.chess.engine.player.BlackPlayer;
+import com.chess.engine.player.Player;
+import com.chess.engine.player.WhitePlayer;
+
 // using an enum primarily because it is typesafe
 public enum Color {
     WHITE {
@@ -17,6 +21,11 @@ public enum Color {
         public boolean isBlack() {
             return false;
         }
+
+        @Override
+        public Player pickPlayer(WhitePlayer whitePlayer, BlackPlayer blackPlayer) {
+            return whitePlayer;
+        }
     },
     BLACK {
         @Override
@@ -33,9 +42,16 @@ public enum Color {
         public boolean isBlack() {
             return true;
         }
+
+        @Override
+        public Player pickPlayer(WhitePlayer whitePlayer, BlackPlayer blackPlayer) {
+            return blackPlayer;
+        }
     };
 
     public abstract int getDirection();
     public abstract boolean isWhite();
     public abstract boolean isBlack();
+
+    public abstract Player pickPlayer(final WhitePlayer whitePlayer, final BlackPlayer blackPlayer);
 }
