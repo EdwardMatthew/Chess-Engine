f70a15c73b099de58327faeb2d692aa08d0de2d5
Edward Matthew <edward.kurniawan@binus.ac.id>
2021-06-15 19:13:28 +0700

    updated methods

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,20 +1,35 @@
 package com.chess.gui;
 
 import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Square;
 import com.chess.engine.board.Utilities;
+import com.chess.engine.pieces.Piece;
+import com.chess.engine.player.MoveTransition;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.swing.SwingUtilities.isLeftMouseButton;
+import static javax.swing.SwingUtilities.isRightMouseButton;
+
 public class Table {
     private final JFrame gameWindow;
     private final BoardPanel boardPanel;
+
+    private Square sourceSquare;
+    private Square destinationSquare;
+    private Piece humanPiece;
+
+    private final Board chessBoard;
     private final Color lightSquare = new Color(227,193,111);
     private final Color darkSquare = new Color(184, 139, 74);
 
@@ -22,12 +37,15 @@ public class Table {
     private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
     private static final Dimension SQUARE_PANEL_DIMENSION = new Dimension(10, 10);
 
+    private static String defaultPieceImageDir = "art/simple/";
+
     public Table() {
         this.gameWindow = new JFrame("Chess");
         final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameWindow.setLayout(new BorderLayout());
         this.gameWindow.setJMenuBar(tableMenuBar);
         this.gameWindow.setSize(OUTER_FRAME_DIMENSION);
+        this.chessBoard = Board.createStandardBoard();
 
         this.boardPanel = new BoardPanel();
         this.gameWindow.add(boardPanel, BorderLayout.CENTER);
@@ -80,6 +98,55 @@ public class Table {
             this.squareId = squareId;
             setPreferredSize(SQUARE_PANEL_DIMENSION);
             setSquareColor();
+            setChessPiece(chessBoard);
+
+            addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    // first click
+                    // cancel picked up piece
+                    if (isRightMouseButton(e)) {
+                        // sourceSquare null == user has not selected piece
+                        // assign piece to human piece
+
+                        sourceSquare = null;
+                        destinationSquare = null;
+                        humanPiece = null;
+                    } else if (isLeftMouseButton(e)) {
+                        if (sourceSquare == null) {
+                            sourceSquare = chessBoard.getSquare(squareId);
+                            humanPiece = sourceSquare.getPiece();
+                            if (humanPiece == null) {
+                                sourceSquare = null;
+                            }
+                        } else {
+                            destinationSquare = chessBoard.getSquare(squareId);
+                            final Move move = null;
+                        }
+                    }
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+
             validate();
         }
 
@@ -100,12 +167,11 @@ public class Table {
         private void setChessPiece(final Board board) {
             this.removeAll();
             if (board.getSquare(this.squareId).isSquareFilled()) {
-                String pieceIconPath = "";
                 try {
                     // name every piece on the file and concatenate .gif on it
                     // also will return the color of the piece
                     // e.g. WhiteBishop -> W
-                    final BufferedImage image = ImageIO.read(new File(pieceIconPath +
+                    final BufferedImage image = ImageIO.read(new File(defaultPieceImageDir +
                             board.getSquare(this.squareId).getPiece().getPieceColor().
                                     toString().substring(0, 1) + board.getSquare(this.squareId).getPiece().toString()
                                     + ".gif"));
